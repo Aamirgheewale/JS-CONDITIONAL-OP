24f2d5b526a0887790e83079ef986d0a73c2048d
Aamirgheewale <aamirgheewale142210@.com>
2025-10-30 10:24:02 +0530

    script: added reset and increment functionality

--- script.js
+++ script.js
@@ -4,12 +4,30 @@ function onDecrement(){
     let previousValue = counterElement.textContent; 
     let updatedValue = parseInt(previousValue) - 1;
     counterElement.textContent = updatedValue;
+    if(updatedValue < 0){
+        counterElement.style.color = `red`;
+    }else if(updatedValue > 0){
+        counterElement.style.color = `green`;
+    }else{
+        counterElement.style.color = `black`;
+    }
 }
 
 function onReset(){
-    
+    let updatedValue = 0;
+    counterElement.textContent = updatedValue;
+    counterElement.style.color = `black`;
 }
 
 function onIncrement(){
-
+    let previousValue = counterElement.textContent; 
+    let updatedValue = parseInt(previousValue) + 1;
+    counterElement.textContent = updatedValue;
+    if(updatedValue < 0){
+        counterElement.style.color = `red`;
+    }else if(updatedValue > 0){
+        counterElement.style.color = `green`;
+    }else{
+        counterElement.style.color = `black`;
+    }
 }
